75c5bb6e89bd91469b7ffca52c9fe51c26ddc5a3
zehara-27 <76928621+zehara-27@users.noreply.github.com>
2021-02-14 13:56:52 -0800

    Amazon Clone

---
+++ src/Chackout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Cheackout.css"
+function Chackout() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Chackout
---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__SearchIcon" />
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Gust</span>
+          <span className="header__optionLineTwo">sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Return</span>
+          <span className="header__optionLineTwo">& Order</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
