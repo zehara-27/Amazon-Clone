6dee37401038799a5f86ed59cd8ab0702c47cedc
zehara-27 <76928621+zehara-27@users.noreply.github.com>
2021-02-16 23:31:18 -0800

    chekout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 
@@ -9,6 +10,9 @@ function App() {
     <Router>
       <div className="App">
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/Checkout">
             <Header />
             <Checkout />
